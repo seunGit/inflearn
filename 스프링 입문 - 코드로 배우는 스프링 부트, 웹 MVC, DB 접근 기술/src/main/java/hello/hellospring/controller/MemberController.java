27ead50054903c17a22660c163f034661897f361
seungit <minsg3669@gmail.com>
2023-03-19 22:12:11 +0900

    update

--- 스프링 입문 - 코드로 배우는 스프링 부트, 웹 MVC, DB 접근 기술/src/main/java/hello/hellospring/controller/MemberController.java
+++ 스프링 입문 - 코드로 배우는 스프링 부트, 웹 MVC, DB 접근 기술/src/main/java/hello/hellospring/controller/MemberController.java
@@ -7,13 +7,13 @@ import org.springframework.stereotype.Controller;
 @Controller
 public class MemberController {
     // 필드 주입 추천x
-    @Autowired private MemberService memberService;
+//    @Autowired private MemberService memberService;
 
     // Setter 주입
-    @Autowired
-    public void setMemberService(MemberService memberService) {
-        this.memberService = memberService;
-    }
+//    @Autowired
+//    public void setMemberService(MemberService memberService) {
+//        this.memberService = memberService;
+//    }
 
     // 생성자 주입 - 추천o
     @Autowired
