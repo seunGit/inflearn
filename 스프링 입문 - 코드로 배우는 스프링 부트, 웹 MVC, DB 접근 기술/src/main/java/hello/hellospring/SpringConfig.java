39d2466cf20d39e5681b72adb8ce59cd822516a9
seungit <minsg3669@gmail.com>
2023-06-24 00:43:40 +0900

    Spring Data JPA

--- 스프링 입문 - 코드로 배우는 스프링 부트, 웹 MVC, DB 접근 기술/src/main/java/hello/hellospring/SpringConfig.java
+++ 스프링 입문 - 코드로 배우는 스프링 부트, 웹 MVC, DB 접근 기술/src/main/java/hello/hellospring/SpringConfig.java
@@ -1,41 +1,18 @@
 package hello.hellospring;
 
-import hello.hellospring.repository.JdbcMemberRepository;
-import hello.hellospring.repository.JdbcTemplateMemberRepository;
-import hello.hellospring.repository.JpaMemberRepository;
-import hello.hellospring.repository.MemberRepository;
+import hello.hellospring.repository.*;
 import hello.hellospring.service.MemberService;
 
-import jakarta.persistence.EntityManager;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.jdbc.core.JdbcTemplate;
-
-import javax.sql.DataSource;
-
-// 자바코드로 직접 스프링 빈 등록하기
 @Configuration
 public class SpringConfig {
-
-    private final DataSource dataSource;
-    private final EntityManager em;
-
-    public SpringConfig(DataSource dataSource, EntityManager em) {
-        this.dataSource = dataSource;
-        this.em = em;
+    private final MemberRepository memberRepository;
+    public SpringConfig(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
     }
-
     @Bean
     public MemberService memberService() {
-        return new MemberService(memberRepository());
+        return new MemberService(memberRepository);
     }
-
-    @Bean
-    public MemberRepository memberRepository() {
-//        return new MemoryMemberRepository();
-//        return new JdbcMemberRepository(dataSource);
-//        return new JdbcTemplateMemberRepository(dataSource);
-        return new JpaMemberRepository(em);
-    }
-
 }
