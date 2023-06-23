a36b474b399fa66d4560d5a067046c12356b8400
seungit <minsg3669@gmail.com>
2023-06-24 08:50:29 +0900

    Start TomcatServer

--- Toby's Spring Boot/src/main/java/spring/helloboot/HellobootApplication.java
+++ Toby's Spring Boot/src/main/java/spring/helloboot/HellobootApplication.java
@@ -1,13 +1,22 @@
 package spring.helloboot;
 
+import org.apache.catalina.startup.Tomcat;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.server.WebServer;
+import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 
-@SpringBootApplication
 public class HellobootApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(HellobootApplication.class, args);
+		// Spring Boot에서 TomcatServletContainer를 내장해서 지원하는 도우미 클래스
+		ServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();
+		WebServer webServer = serverFactory.getWebServer();
+		webServer.start(); // 톰캣 서버까지 띄워보기
+
+
+
 	}
 
 }
