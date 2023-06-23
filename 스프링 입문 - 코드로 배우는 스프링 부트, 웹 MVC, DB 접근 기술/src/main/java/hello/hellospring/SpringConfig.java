9949ffba7417798d1b31d2c3f76a3b2e91557c14
DESKTOP-6QAT7A2\승기 <minsg3669@gmail.com>
2023-06-23 23:56:47 +0900

    Revert "JPA"
    This reverts commit 8699ff9aab19f0eb4b764de1df1d795ac9508599.

--- 스프링 입문 - 코드로 배우는 스프링 부트, 웹 MVC, DB 접근 기술/src/main/java/hello/hellospring/SpringConfig.java
+++ 스프링 입문 - 코드로 배우는 스프링 부트, 웹 MVC, DB 접근 기술/src/main/java/hello/hellospring/SpringConfig.java
@@ -2,12 +2,9 @@ package hello.hellospring;
 
 import hello.hellospring.repository.JdbcMemberRepository;
 import hello.hellospring.repository.JdbcTemplateMemberRepository;
-import hello.hellospring.repository.JpaMemberRepository;
 import hello.hellospring.repository.MemberRepository;
 import hello.hellospring.service.MemberService;
 
-import jakarta.persistence.EntityManager;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.jdbc.core.JdbcTemplate;
@@ -18,11 +15,10 @@ import javax.sql.DataSource;
 @Configuration
 public class SpringConfig {
 
-    private EntityManager em;
+    private final DataSource dataSource;
 
-    @Autowired
-    public SpringConfig(EntityManager em) {
-        this.em = em;
+    public SpringConfig(DataSource dataSource) {
+        this.dataSource = dataSource;
     }
 
     @Bean
@@ -34,9 +30,7 @@ public class SpringConfig {
     public MemberRepository memberRepository() {
 //        return new MemoryMemberRepository();
 //        return new JdbcMemberRepository(dataSource);
-//        return new JdbcTemplateMemberRepository(dataSource);
-        return new JpaMemberRepository(em);
-
+        return new JdbcTemplateMemberRepository(dataSource);
     }
 
 }
--- 스프링 입문 - 코드로 배우는 스프링 부트, 웹 MVC, DB 접근 기술/src/main/java/hello/hellospring/damain/Member.java
+++ 스프링 입문 - 코드로 배우는 스프링 부트, 웹 MVC, DB 접근 기술/src/main/java/hello/hellospring/damain/Member.java
@@ -1,14 +1,7 @@
 package hello.hellospring.damain;
 
-import jakarta.persistence.*;
-
-@Entity // Jpa가 관리하는 엔티티
 public class Member {
-
-    @Id // PK
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
     private String name;
 
     public Long getId() {
