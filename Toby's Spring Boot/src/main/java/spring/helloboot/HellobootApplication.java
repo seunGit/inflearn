d29e2d5a163dc480dcaadd3ae744e7e778c7b9b8
seungit <minsg3669@gmail.com>
2023-06-24 09:27:59 +0900

    FrontController setting

--- Toby's Spring Boot/src/main/java/spring/helloboot/HellobootApplication.java
+++ Toby's Spring Boot/src/main/java/spring/helloboot/HellobootApplication.java
@@ -9,6 +9,7 @@ import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactor
 import org.springframework.boot.web.server.WebServer;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 
@@ -20,22 +21,28 @@ public class HellobootApplication {
 		// Spring Boot에서 TomcatServletContainer를 내장해서 지원하는 도우미 클래스
 		ServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();
 		WebServer webServer = serverFactory.getWebServer(servletContext -> {
-			servletContext.addServlet("hello", new HttpServlet() {
+			servletContext.addServlet("frontController", new HttpServlet() {
 				@Override
 				protected void service(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
-					// name parameter 받기
-					String name = req.getParameter("name");
+					//프론트 컨트롤러 - 인증, 보안, 다국어처리, 공통 기능 등등
+					// http -v POST ":8080/hello?name=spring" , POST 요청시 404에러
+					if (req.getRequestURI().equals("/hello") && req.getMethod().equals(HttpMethod.GET.name())) {
+						// name parameter 받기
+						String name = req.getParameter("name");
 
-					res.setStatus(HttpStatus.OK.value()); // res.setStatus(200); 응답코드
-//					res.setHeader("Content-Type", "text/plain") // 스프링에서 제공해주는 ENUM 사용
-					res.setHeader(HttpHeaders.CONTENT_TYPE, MediaType.TEXT_PLAIN_VALUE); // 콘텐츠타입 헤더
-					res.getWriter().println("Hello " + name); // 바디
+						res.setStatus(HttpStatus.OK.value()); // 응답코드
+						res.setHeader(HttpHeaders.CONTENT_TYPE, MediaType.TEXT_PLAIN_VALUE); // 콘텐츠타입 헤더
+						res.getWriter().println("Hello " + name); // 바디
+					} else if (req.getRequestURI().equals("/user")) {
+
+					} else {
+						res.setStatus(HttpStatus.NOT_FOUND.value());
+					}
 				}
-			}).addMapping("/hello");
+			}).addMapping("/*");
 
 		});
 		webServer.start(); // 톰캣 서버까지 띄워보기
-
 
 
 	}
