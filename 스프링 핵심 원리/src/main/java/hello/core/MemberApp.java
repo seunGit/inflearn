97617b25313985fb1c6f91c016d1911c7f55d9b5
seungit <minsg3669@gmail.com>
2023-06-26 19:18:35 +0900

    AppConfig 설정을 통한 관심사 분리, 리팩토링

--- 스프링 핵심 원리/src/main/java/hello/core/MemberApp.java
+++ 스프링 핵심 원리/src/main/java/hello/core/MemberApp.java
@@ -1,5 +1,6 @@
 package hello.core;
 
+import hello.core.config.AppConfig;
 import hello.core.member.Grade;
 import hello.core.member.Member;
 import hello.core.member.MemberService;
@@ -9,7 +10,8 @@ import java.util.Arrays;
 
 public class MemberApp {
     public static void main(String[] args) {
-        MemberService memberService = new MemberServiceImpl();
+        AppConfig appConfig = new AppConfig();
+        MemberService memberService = appConfig.memberService();
         Member member = new Member(1L, "memberA", Grade.VIP);
         memberService.join(member);
 
---
+++ 스프링 핵심 원리/src/main/java/hello/core/config/AppConfig.java
@@ -0,0 +1,30 @@
+package hello.core.config;
+
+import hello.core.discount.DiscountPolicy;
+import hello.core.discount.FixDiscountPolicy;
+
+import hello.core.member.MemberService;
+import hello.core.member.MemberServiceImpl;
+import hello.core.member.MemoryMemberRepository;
+
+import hello.core.order.OrderService;
+import hello.core.order.OrderServiceImpl;
+
+public class AppConfig {
+    public MemberService memberService() {
+        return new MemberServiceImpl(memberRepository());
+    }
+
+    public OrderService orderService() {
+        return new OrderServiceImpl(memberRepository(), discountPolicy());
+
+    }
+
+    private static MemoryMemberRepository memberRepository() {
+        return new MemoryMemberRepository();
+    }
+
+    public DiscountPolicy discountPolicy(){
+        return new FixDiscountPolicy();
+    }
+}
